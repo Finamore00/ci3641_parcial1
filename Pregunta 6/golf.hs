d49d38640a2f7f42ee2b4d9ab97ce9db484e600f
Santiago Finamore <18-10125@usb.ve>
2023-10-13 19:16:11 -0400

    Implementada Función watafok Pregunta 6. Velocidad satisfactoria para n=100

---
+++ Pregunta 6/golf.hs
@@ -0,0 +1,10 @@
+import Prelude
+fi x _ 1 = x
+fi _ _ 0 = 0
+fi x y k = fi (x+y) x (k-1)
+fa 0 = 1
+fa n = n*fa (n-1)
+bc n k = if k > n then 0 else div (fa n) ((fa k)*(fa (n-k)))
+na n k = div ((bc n k)*(bc n (k-1))) n
+lf = floor . logBase 2 . fromIntegral
+w n = fi 1 0 (lf (na (n+1) (n-1)) + 1)
