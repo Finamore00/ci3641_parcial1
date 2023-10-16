030b35009e8308948e7e5c8825657aa5bb965839
Santiago Finamore <18-10125@usb.ve>
2023-10-15 20:19:53 -0400

    Insertados parentesis en golf.py

--- Pregunta 6/golf.py
+++ Pregunta 6/golf.py
@@ -2,4 +2,4 @@ import sys
 from math import *
 from sympy import *
 n = int(sys.argv[1])
-print(fibonacci(floor(log2(((n+1)**2)*n*(n-1)/12))+1))
+print(fibonacci(floor(log2((((n+1)**2)*n*(n-1))/12))+1))
